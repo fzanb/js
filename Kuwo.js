2d5231a60ad4d89dc160a1121a0184f5690cb528
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-14 16:21:41 +0800

    Create Kuwo.js

---
+++ Kuwo.js
@@ -0,0 +1,7 @@
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+
+[mitm]
+hostname = vip1.kuwo.cn
