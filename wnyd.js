a36a63698999454c468bbb47ec797a5070733b54
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-14 16:39:31 +0800

    Create wnyd.js

---
+++ wnyd.js
@@ -0,0 +1,7 @@
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/fzanb/js/main/wnyd.js
+
+[mitm]
+hostname = p.du.163.com
