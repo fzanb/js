d0c78c48193f5cdb7c83987a72e1f4eea0d3c138
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-14 17:04:57 +0800

    Create Spotify.js

---
+++ Spotify.js
@@ -0,0 +1,4 @@
+================QuantumultX================
+
+[rewrite_local]
+^https\:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/fzanb/js/main/Spotify.js
