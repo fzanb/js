b886b4127f0eb167f152443f842b9741b8c497cf
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-14 23:38:00 +0800

    Update Kuwo.js

--- Kuwo.js
+++ Kuwo.js
@@ -5,3 +5,31 @@ QuantumultX:
 
 [mitm]
 hostname = vip1.kuwo.cn
+**************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/vip/v2/user/vip';
+const time = '/vip/spi/mservice';
+
+if (url.indexOf(vip) != -1) {
+	obj.data["isNewUser"] = "2";
+	obj.data["vipLuxuryExpire"] = "4102415999000";
+	obj.data["time"] = "4102415999000";
+	obj.data["isYearUser"] = "2";
+	obj.data["vipmExpire"] = "4102415999000";
+	obj.data["vipOverSeasExpire"] = "4102415999000";
+	obj.data["vipExpire"] = "4102415999000";
+	obj.data["vip3Expire"] = "4102415999000";
+	body = JSON.stringify(obj);
+}
+
+if (url.indexOf(time) != -1) {
+	obj["isVIPMAutoPay"] = 2;
+	obj["isVIPLuxAutoPay"] = 2;
+	body = JSON.stringify(obj);
+}
+
+$done({body});
