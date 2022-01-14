26b8abe26b71f6530d0963ab1b41f57b640b6bc6
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-14 16:26:51 +0800

    Update Kuwo.js

--- Kuwo.js
+++ Kuwo.js
@@ -1,7 +1,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/fzanb/js/main/Kuwo.js
 
 [mitm]
 hostname = vip1.kuwo.cn
