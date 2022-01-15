70481761e8c71437697981f2bda7b479f20f72f1
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-15 10:45:11 +0800

    Update Wps.js

--- Wps.js
+++ Wps.js
@@ -1,3 +1,7 @@
+/*
+WPS Office 解锁部分功能
+
+***************************
 QuantumultX:
 
 [rewrite_local]
@@ -5,3 +9,37 @@ QuantumultX:
 
 [mitm]
 hostname = account.wps.cn
+**************************/
+
+var body = JSON.parse($response.body);
+var obj = {
+  exp: 0,
+  level: 3,
+  privilege: [
+    { spid: "data_recover", times: 0, expire_time: 4102415999 },
+    { spid: "ocr", times: 0, expire_time: 4102415999 },
+    { spid: "pdf2doc", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_merge", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_sign", times: 0, expire_time: 4102415999 },
+    { spid: "pdf_split", times: 0, expire_time: 4102415999 }
+  ],
+  result: "ok",
+  total_buy: 0,
+  total_cost: -30,
+  userid: body.userid,
+  vip: {
+    name: "超级会员",
+    has_ad: 0,
+    memberid: 40,
+    expire_time: 4102415999,
+    enabled: [
+      { memberid: 40, name: "超级会员", expire_time: 4102415999 },
+      { memberid: 20, name: "WPS会员", expire_time: 4102415999 },
+      { memberid: 12, name: "稻壳会员", expire_time: 4102415999 }
+    ]
+  },
+  wealth: 0,
+  expire_time: 4102415999
+};
+
+$done({ body: JSON.stringify(obj) });
