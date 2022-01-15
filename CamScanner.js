fa6c9ac17cedc7b6b232f9ecb810ff7116785dfa
fzanb <96323630+fzanb@users.noreply.github.com>
2022-01-15 10:43:46 +0800

    Update CamScanner.js

--- CamScanner.js
+++ CamScanner.js
@@ -1,6 +1,14 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
 Quantumult X:
 
 [rewrite_local]
 ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/fzanb/js/main/CamScanner.js
 [mitm]
 hostname = ap*.intsig.net
+**************************/
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"4102415999"}}};
+$done({body: JSON.stringify(obj)});
