e00a4fd315c27389035471c1a90555706d179e76
fzanb <96323630+fzanb@users.noreply.github.com>
2023-05-07 16:30:02 +0800

    Update foxus.js

--- foxus.js
+++ foxus.js
@@ -2,32 +2,7 @@
   
 # > foco
 ^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/fzanb/js/main/foxus.js 
-
-
 [mitm]
- 
 hostname = focos.oracle.bendingspoonsapps.com
-
-
 ***********************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var obj=JSON.parse($response.body);obj.me.active_subscriptions_ids=["com.focos.1w_t8_1w"],$done({body:JSON.stringify(obj)});
